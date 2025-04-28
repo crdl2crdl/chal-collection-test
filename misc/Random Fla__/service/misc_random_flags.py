df4f35cc2b3363927a3cc6e13eb9e767ab1491d9
Dafydd Marks <marksdafydd@gmail.com>
2025-04-28 12:12:52 +0800

    Modify rand flag chal params

--- misc/Random Fla__/service/misc_random_flags.py
+++ misc/Random Fla__/service/misc_random_flags.py
@@ -2,13 +2,13 @@
 
 import random
 
-flag = [0]*700
+flag = [0]*650
 
 with open("flag.txt","rb") as f:
     hidden = f.read()
     for _ in range(len(hidden)):
         flag[_] = hidden[_]
 
-for _ in range(700):
+for _ in range(650):
     print(random.getrandbits(32)+flag[_])
     input()
